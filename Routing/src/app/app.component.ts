7559a7e3723e845ee8b62b981c828bdf0d057e8f
mondli16 <125990471+mondli16@users.noreply.github.com>
2024-12-11 11:11:48 +0200

    Add files via upload

---
+++ Routing/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
+  template: `
+    <nav>
+      <a href="/">Home</a>
+      |
+      <a href="/user">User</a>
+    </nav>
+    <router-outlet />
+  `,
+})
+export class AppComponent { }
---
+++ Routing/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {UserComponent} from './user/user.component';
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'App Home Page',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    title: 'App User Page',
+    component: UserComponent,
+  },
+];
